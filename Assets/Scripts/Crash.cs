2c66cdc735d5808b2c6e9c15deced88ece0a51fb
michael <michael@xtendrobotics.com>
2024-12-08 01:02:41 +0200

    Initial commit

---
+++ Assets/Scripts/Crash.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Crash : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float ImpactCapacity;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocity = rb.linearVelocity;
+        Debug.Log("Velocity: "+ velocity.magnitude);
+        if(velocity.magnitude > ImpactCapacity)
+        {
+            Debug.Log("Crash!");
+        }
+    }
+}
---
+++ Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpaceshipController : MonoBehaviour
+{
+    //private bool HitFloor = false;
+    [Tooltip("Thrust strength")]
+    [SerializeField] float ThrustStrength = 1f;
+    [Tooltip("Left and Right Torque strength")]
+    [SerializeField] float TorqueStrength = 1f;
+
+
+    [SerializeField]
+    InputAction move = new InputAction(type: InputActionType.Value, expectedControlType: nameof(Vector2));
+
+    private Rigidbody2D rb;
+
+    void OnEnable()
+    {
+        move.Enable();
+    }
+
+    void OnDisable()
+    {
+        move.Disable();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {   
+        rb.AddForce(transform.up * ThrustStrength * move.ReadValue<Vector2>().y);
+        rb.AddTorque(TorqueStrength * -move.ReadValue<Vector2>().x); // Around Z axis unless specified athorwise
+    }
+}
